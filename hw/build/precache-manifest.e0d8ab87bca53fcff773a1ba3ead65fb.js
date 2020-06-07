feca76647d9182dbbe4ef9a1178c835e3101dab1
nathanKith <ntolparov@gmail.com>
2020-06-07 22:48:19 +0300

    add homework

---
+++ hw/build/precache-manifest.e0d8ab87bca53fcff773a1ba3ead65fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a074dedb73dbf31d920aa83ab84784ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaee717773892e8fd88f",
+    "url": "/static/css/main.c0eaf84d.chunk.css"
+  },
+  {
+    "revision": "c580d3507691fdd9f58b",
+    "url": "/static/js/2.58a07eea.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.58a07eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaee717773892e8fd88f",
+    "url": "/static/js/main.535eeae3.chunk.js"
+  },
+  {
+    "revision": "ca325759bef855b055d5",
+    "url": "/static/js/runtime-main.d92c1b2a.js"
+  }
+]);
